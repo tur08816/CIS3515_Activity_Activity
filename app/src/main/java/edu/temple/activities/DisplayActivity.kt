95692a2a113eb31617e5d9e9f3179d02a73ec3a8
James McMichael <tur08816@temple.edu>
2025-09-30 14:52:43 -0400

    Second activity launches with text size set to number clicked in main activity

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -11,7 +11,8 @@ class DisplayActivity : AppCompatActivity() {
 
         // TODO Step 3: Extract transferred value and use for lyricsDisplayView text size
         with (findViewById<TextView>(R.id.lyricsDisplayTextView)) {
-
+            val size = intent.getIntExtra("TextSize",10)
+            textSize = size.toFloat()
         }
 
 
--- app/src/main/java/edu/temple/activities/MainActivity.kt
+++ app/src/main/java/edu/temple/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -24,11 +25,14 @@ class MainActivity : AppCompatActivity() {
 
             // TODO Step 2: Implement lambda body to launch new activity and pass value
             adapter = TextSizeAdapter(textSizes){
-
+                textSizes ->
+                val intent = Intent(this@MainActivity, DisplayActivity::class.java)
+                intent.putExtra("TextSize", textSizes)
+                startActivity(intent)
             }
             layoutManager = LinearLayoutManager(this@MainActivity)
-        }
 
+        }
 
 
     }
@@ -36,12 +40,15 @@ class MainActivity : AppCompatActivity() {
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+class TextSizeAdapter (private val textSizes: Array<Int>, private val callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
     // TODO Step 1: Complete onClickListener to return selected number
+
     inner class TextSizeViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {
         init {
-            textView.setOnClickListener {  }
+            textView.setOnClickListener {
+                callback(textSizes[adapterPosition])
+            }
         }
     }
 
@@ -53,6 +60,7 @@ class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit)
         holder.textView.apply {
             text = textSizes[position].toString()
             textSize = textSizes[position].toFloat()
+
         }
     }
 
